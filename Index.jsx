b4f53ceb878e55da93e164bd5c769f2a2828de59
Nwakodo Ifeanyi <46559372+ify12345@users.noreply.github.com>
2024-04-01 18:19:38 +0100

    CORRECTIONS

--- Index.jsx
+++ Index.jsx
@@ -1,24 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import OnboardingScreen from './screens/OnboardingScreen';
+import OnboardingScreen from './screens/onboarding/OnboardingScreen';
 import Home from './screens/Home';
 import { useState,useEffect } from 'react';
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from 'react';
 import React from 'react';
 import Form from './screens/Form';
 import Account from './screens/Account';
 import Form2 from './screens/Form2';
 import SignIn from './screens/SignIn';
+
+
+
 const Stack = createStackNavigator()
 
 export default function Index() {
+  const [fontsLoaded] = useFonts({
+    GeneralSans: require("./assets/fonts/GeneralSans-Regular.ttf"),
+    GeneralSansMedium: require("./assets/fonts/GeneralSans-Medium.ttf"),
+    GeneralSansSemibold: require("./assets/fonts/GeneralSans-Semibold.ttf"),
+  })
 
+
+  const onLayoutRootView = useCallback(async()=>{
+    if(fontsLoaded){
+      await SplashScreen.hideAsync();
+
+    }
+    if(!fontsLoaded){
+      return null
+    }
+  })
   return (
   
       <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+      <Stack.Navigator>
+        <Stack.Screen
+        name="Onboarding"
+        component={OnboardingScreen}
+        options={{ headerShown: false }}
+        />
+        
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Form" component={Form} />
         <Stack.Screen name="Form2" component={Form2} />
@@ -31,5 +57,7 @@ export default function Index() {
 }
 
 const styles = StyleSheet.create({
-
+    container:{
+      fontFamily: "GeneralSansSemibold",
+    }
 });
--- screens/onboarding/OnboardingScreen.jsx
+++ screens/onboarding/OnboardingScreen.jsx
@@ -11,14 +11,15 @@ import {
   ScrollView,
 } from "react-native";
 import { useState } from "react";
-import React from "react";
-const { width, height } = Dimensions.get("window");
+import React from "react"
+import { width,height } from "../../assets/constants";
+import styles from "./onboarding.style"
 const COLORS = { white: "green" };
 
 const slides = [
   {
     id: "1",
-    image: require("../assets/global.png"),
+    image: require("../../assets/global.png"),
     title: "Global trading platform for recycled plastic",
     subtitle:
       "We enable buyers and sell to efficiently trade recycled plastics with confidence, bringing increased opportunity for both party.",
@@ -26,14 +27,14 @@ const slides = [
 
   {
     id: "2",
-    image: require("../assets/plasticbales.png"),
+    image: require("../../assets/plasticbales.png"),
     title: "Plastic Bales",
     subtitle:
       "Bales trading on the RPX are primarily any whole polyethylene terephthalate (PET)bottle with a screws-neck top that contains",
   },
   {
     id: "3",
-    image: require("../assets/sustainable.png"),
+    image: require("../../assets/sustainable.png"),
     title: "Sustainable Global Hub for Recycled Plastics",
     subtitle:
       "We are a global marketplace for recycled plastic, with a wide range of plastic types and grades available",
@@ -42,10 +43,12 @@ const slides = [
 
 const OnboardingScreen = ({ navigation }) => {
   const [currentSlide, setCurentSlide] = React.useState(0);
+
   const ref = React.useRef(null);
+
   const Header = () => {
     return (
-      <View>
+      <View style={styles.bars}>
         <View style={{ justifyContent: "space-between" }}>
           <View
             style={{
@@ -88,26 +91,17 @@ const OnboardingScreen = ({ navigation }) => {
   const Slide = ({ item }) => {
     return (
       <View
-        style={{
-          alignItems: "center",
-          width,
-          flex: 1,
-          flexDirection: "column",
-          justifyContent: "space-between",
-        }}
+        style={styles.imgContainer}
       >
         <View
           style={{ alignItems: "center", flexDirection: "column", gap: 30 }}
         >
           <View
-            style={{
-              paddingVertical: 37,
-              marginTop: 5,
-              width,
-              alignItems: "center",
-            }}
+            style={
+              styles.container1
+          }
           >
-            <Image source={item.image} style={{ width: 380, height: 380 }} />
+            <Image source={item.image} style={{ width: 350, height: 350 }} />
           </View>
           <View style={{ flexDirection: "column", gap: 10 }}>
             <Text style={styles.title}>{item.title}</Text>
@@ -128,53 +122,31 @@ const OnboardingScreen = ({ navigation }) => {
           <TouchableOpacity style={[styles.btnContainer]} onPress={goNext}>
             <Text style={{ color: "white", textAlign: "center" }}>Next</Text>
           </TouchableOpacity>
-        )}
+        )
+        }
       </View>
     );
   };
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+    <SafeAreaView style={styles.generalContainer}>
+    
       <Header />
-      <FlatList
-        data={slides}
-        horizontal
-        ref={ref}
-        onMomentumScrollEnd={updateSlide}
-        pagingEnabled
-        showsHorizontalScrollIndicator={false}
-        renderItem={({ item }) => <Slide item={item} />}
-      />
+      <ScrollView>
+        <FlatList
+          data={slides}
+          horizontal
+          ref={ref}
+          onMomentumScrollEnd={updateSlide}
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          renderItem={({ item }) => <Slide item={item} />}
+        />
+      </ScrollView>
+      
     </SafeAreaView>
   );
 };
-const styles = StyleSheet.create({
-  title: {
-    fontSize: 24,
-    fontWeight: "700",
-    maxWidth: 270,
-    textAlign: "center",
-  },
-  subtitle: {
-    fontSize: 14,
-    fontWeight: "400",
-    maxWidth: 270,
-    textAlign: "center",
-    lineHeight: 20,
-  },
-  indicator: {
-    height: 2,
-    width: "30%",
-    alignItems: "center",
-    backgroundColor: "#f5f5f5",
-  },
-  btnContainer: {
-    backgroundColor: "black",
-    borderRadius:3,
-    width: "90%",
-    padding: 20,
-    color: "white",
-  },
-});
+
 
 export default OnboardingScreen;
